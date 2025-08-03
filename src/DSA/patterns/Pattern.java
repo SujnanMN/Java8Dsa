f2c199944eabe9a556c11c5574117acff0b3354c
SujnanMN <sujnansms99@gmail.com>
2025-08-03 21:12:01 +0530

    Initial commit

---
+++ src/DSA/patterns/Pattern.java
@@ -0,0 +1,110 @@
+package DSA.patterns;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Pattern {
+
+    public static void main(String[] args) {
+        //patternSeven();
+        //System.out.println(isPalindrome(121));
+        //System.out.println(isPrime(29));
+        revArrayRecursion(null);
+    }
+
+    public static void patternFour() {
+        int n = 5;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print(i + 1 + " ");
+            }
+            System.out.println("");
+        }
+    }
+
+    public static void patternSeven() {
+        int n=5;
+        int k=1;
+        for  (int i=0; i<n; i++) {
+            for (int j=0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j=0; j<k; j++) {
+                System.out.print("*");
+            }
+            k+=2;
+            System.out.println("");
+        }
+    }
+
+    public static void patternNine() {
+        int n=5;
+        int k=1;
+        for  (int i=0; i<n; i++) {
+            for (int j=0; j<n-i-1; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j=0; j<k; j++) {
+                System.out.print("*");
+            }
+            k+=2;
+            System.out.println("");
+        }
+    }
+
+    private static boolean isPalindrome(int num) {
+        int rev=0;
+        int temp=num;
+        while(num!=0) {
+            int dig = num%10;
+            rev = rev *10+dig;
+            num=num/10;
+        }
+
+        System.out.println(rev);
+        return temp == rev;
+    }
+
+    private static String isPrime(int num) {
+        if (num<=0)
+            return "NA";
+        else if (num==1)
+                return "Prime";
+        else
+        for(int i=2; i<=Math.sqrt(num); i++) {
+            if (num%i==0)
+                return "Composite";
+        }
+        return "Prime";
+    }
+
+
+    private static int[] revArrayRecursion(int[] arr) {
+         recursion(5);
+         return null;
+    }
+
+    private static void recursion (int n) {
+        //int i=0;
+        if (true)
+            return;
+        else {
+            System.out.println(n);
+
+            recursion(n-1);
+        }
+    }
+
+    public static void interviewQ(String[] str) {
+        Integer[] arr = {1000, 2000, 3000, 30000};
+        List<Integer> list = Arrays.asList(arr);
+        list = list.stream().filter(x -> x < 10000).map(x -> x + x / 10).toList();
+        //.stream().filter(x-> x<20000)
+        //.map(y->y=y+y/10).toList();
+        System.out.println(list);
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.List;
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        String str = "This's!!& is a new String";
+        str = str.replaceAll("[^a-zA-Z0-9\\s]","");
+
+        String[] strArr = str.split("\\s+");
+        Arrays.sort(strArr);
+        System.out.println(Arrays.toString(strArr));
+        Arrays.sort(strArr, (a,b) -> Integer.compare(b.length(), a.length()));
+        System.out.println(Arrays.toString(strArr));
+        //str.replaceAll()
+    }
+}
